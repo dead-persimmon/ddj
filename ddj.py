ccdb8ec1ada8b221ab9b960df1697608d31d92c1
dead-persimmon <kosyrin@gmail.com>
2014-08-19 05:37:47 -0400

    refactoring

--- ddj.py
+++ ddj.py
@@ -1,26 +1,49 @@
 from tkinter import *
 from tkinter import ttk
 
-WORK_DIR = 'C:\\~ Downloads'
-SPECIAL_TITLES = []
+root = Tk() # has to be here
 
-GROUP_TO_FILE_ID = {}
-FILE_TO_GROUP_ID = {}
+class State:
+    def __init__(self):
+    
+        self.default_work_dir = 'C:\\~ Downloads'
+        
+        self.console_text = StringVar()
+        self.work_dir_text = StringVar()
+        
+        self.work_dir_text.set(self.default_work_dir)
+        
+        self.special_titles = []
+        with open('special_titles.txt', 'r') as file:
+            for line in file:
+                if line and line[0] == '#':
+                    continue
+                tokens = line.split('::')
+                tokens = [x.strip() for x in tokens]
+                if len(tokens) == 1:
+                    self.special_titles.append({ 'search_term': tokens[0], 'tidy_title': tokens[0] })
+                elif len(tokens) == 2:
+                    self.special_titles.append({ 'search_term': tokens[0], 'tidy_title': tokens[1] })
+            
+        self.files_by_id = {}
 
-SELECTED_GROUPS = None
-SELECTED_FILES = None
+    def get_work_dir(self):
+        global work_dir_text
+        return work_dir_text.get()
 
-with open('special_titles.txt', 'r') as file:
-    for line in file:
-        if line and line[0] == '#': continue
-        tokens = line.split('::')
-        tokens = [x.strip() for x in tokens]
-        if len(tokens) == 1:
-            SPECIAL_TITLES.append({ 'search_term': tokens[0], 'tidy_title': tokens[0] })
-        elif len(tokens) == 2:
-            SPECIAL_TITLES.append({ 'search_term': tokens[0], 'tidy_title': tokens[1] })
-        else:
-            continue
+    def selected_files(self):
+        global tree
+        file_ids = set()
+        for item_id in tree.selection():
+            children_ids = tree.get_children(item_id)
+            if children_ids: # it's a group
+                file_ids |= set(children_ids)
+            else: # it's a single file
+                file_ids.add(item_id)
+        for file_id in file_ids:
+            yield file_id
+
+state = State()
 
 def build_normalize_title():
     import re
@@ -74,8 +97,66 @@ def build_normalize_title():
 
 normalize_title = build_normalize_title()
 
+def clear_tree():
+    global tree
+    for item_id in tree.get_children():
+        tree.delete(item_id)
+
+def update_tree():
+    global state, tree
+    
+    groups = {}
+    
+    work_dir = state.work_dir_text.get()
+
+    import os
+    from itertools import product
+    
+    for entry in os.listdir(work_dir):
+        path = os.path.join(work_dir, entry)
+        if not os.path.isfile(path):
+            continue
+        normalized_title, episode_number = normalize_title(entry)
+        for special_title, title in product(state.special_titles, [normalized_title.lower()]):
+            if special_title['search_term'].lower() in title:
+                title = special_title['tidy_title']
+                break
+        else:
+            title = normalized_title
+        
+        groups.setdefault(title, { 'title': title, 'files': [], 'path': path, 'size': 0, 'episodes': set(), 'episodes_text': str() })
+        
+        if episode_number:
+            groups[title]['episodes'].add(episode_number)
+            
+        file = { 'name': entry, 'path': path, 'size': os.stat(path).st_size // 1024**2 }
+        
+        groups[title]['files'].append(file)
+        groups[title]['size'] += file['size']
+        
+    for group in sorted(groups.values(), reverse = True, key = lambda x: x['size']):
+        episodes = list(group['episodes'])
+        if episodes:
+            ranges = []
+            index_a = 0
+            for index_b, (a, b) in enumerate(zip(episodes, episodes[1:] + [episodes[-1]])):
+                if b - a != 1:
+                    ranges.append((index_a, index_b))
+                    index_a = index_b + 1
+            group['episodes_text'] = ' (%s)' % ', '.join(
+                '%d~%d' % (episodes[index_a], episodes[index_b])
+                if index_b - index_a
+                else str(episodes[index_a])
+                for index_a, index_b in ranges
+            )
+        
+        group_id = tree.insert('', END, text = group['title'] + group['episodes_text'], values = (len(group['files']), group['size']))
+        
+        for file in group['files']:
+            file_id = tree.insert(group_id, END, text = file['name'], values = (1, file['size']))
+            state.files_by_id[file_id] = file
+
 # main window
-root = Tk()
 root.title('Downloads Directory Janitor 0.1')
 root.geometry('640x720')
 root.config(padx = 8, pady = 8)
@@ -86,21 +167,8 @@ root.rowconfigure(1, weight = 1)
 top_frame = ttk.Frame(root)
 top_frame.grid(column = 0, columnspan = 2, row = 0, sticky = (W, E), pady = (0, 8))
 
-def refresh_button_click():
-    global work_dir_text, console_text, tree
-    
-    work_dir = work_dir_text.get()
-    
-    import os
-    if not os.path.isdir(work_dir):
-        console_text.set('(%s) does not appear to be an existing directory.' % work_dir)
-        return None
-
-    populate_tree(tree)
-
 refresh_button = ttk.Button(top_frame, text = 'Refresh')
 refresh_button.pack(side = RIGHT, padx = (8, 0))
-refresh_button.config(command = refresh_button_click)
 
 work_dir = ttk.Entry(top_frame)
 work_dir.pack(side = RIGHT, fill = X, expand = 1)
@@ -133,129 +201,55 @@ buttons_frame.grid(column = 0, columnspan = 2, row = 3, sticky = (W, E))
 
 delete_button = ttk.Button(buttons_frame, text = 'Delete')
 delete_button.pack(side = RIGHT, padx = (8, 0), pady = (8, 0))
-def delete_button_click():
-    def delete_file(group_id, file_id):
-        import os, stat
-        global GROUP_TO_FILE_ID, tree, console_text
-        file = GROUP_TO_FILE_ID[group_id][file_id]
-        try:
-            #console_text.set('Attempting to delete (%s).' % file['path'])
-            os.chmod(file['path'], stat.S_IWRITE)
-            os.remove(file['path'])
-            return (1, file['size'])
-        except Exception as e:
-            print(e)
-            return (0, 0)
+
+# handlers
+def on_tree_selection_update(event):
+    global state
+    state.console_text.set('Selected %d files.' % len(list(state.selected_files())))
+def on_refresh_button_click():
+    global state, tree
+    clear_tree()
     
-    global tree, SELECTED_FILES, SELECTED_GROUPS, GROUP_TO_FILE_ID, console_text
+    work_dir = state.work_dir_text.get()
+    
+    import os
+    if not os.path.isdir(work_dir):
+        state.console_text.set('(%s) does not appear to be an existing directory.' % work_dir)
+        return None
+
+    update_tree()
+def on_delete_button_click():
+    import os, stat
+    global state
     
     files_counter = 0
     size_counter = 0
     
-    for file_id in SELECTED_FILES:
-        files, size = delete_file(tree.parent(file_id), file_id)
-        files_counter += files
-        size_counter += size
-        #tree.delete(file_id)
-    for group_id in SELECTED_GROUPS:
-        for file_id in tree.get_children(group_id):
-            files, size = delete_file(group_id, file_id)
-            files_counter += files
-            size_counter += size
-            #tree.delete(file_id)
-        tree.delete(group_id)
-    console_text.set('Deleted %d files, %d Mb.' % (files_counter, size_counter))
-    refresh_button_click()
-    
-delete_button.config(command = delete_button_click)
-
-#button2 = ttk.Button(buttons_frame, text = 'B2').pack(side = RIGHT, padx = (8, 0), pady = (8, 0))
-
-# vars
-console_text = StringVar()
-
-work_dir_text = StringVar()
-work_dir_text.set(WORK_DIR)
+    for file_id in state.selected_files():
+        file = state.files_by_id[file_id]
+        try:
+            os.chmod(file['path'], stat.S_IWRITE)
+            os.remove(file['path'])
+        except Exception as exception:
+            print(exception)
+        else:
+            files_counter += 1
+            size_counter += file['size']
+            
+    state.console_text.set('Deleted %d files, %d Mb.' % (files_counter, size_counter))
+    on_refresh_button_click()
 
 # bindings
 tree.config(yscrollcommand = scrollbar.set)
 scrollbar.config(command = tree.yview)
 
-console.config(textvariable = console_text)
+console.config(textvariable = state.console_text)
+work_dir.config(textvariable = state.work_dir_text)
 
-work_dir.config(textvariable = work_dir_text)
-
-def populate_tree(tree):
-    import os
-    from itertools import product
-
-    global GROUP_TO_FILE_ID, FILE_TO_GROUP_ID
-    GROUP_TO_FILE_ID.clear()
-    FILE_TO_GROUP_ID.clear()
-
-    groups = {}
-    
-    for item_id in tree.get_children():
-        tree.delete(item_id)
-
-    global work_dir_text
-    work_dir = work_dir_text.get()
-
-    for entry in os.listdir(work_dir):
-        path = os.path.join(work_dir, entry)
-        if not os.path.isfile(path):
-            continue
-        
-        title, episode_number = normalize_title(entry)
-        for special_title, title in product(SPECIAL_TITLES, [title]):
-            if special_title['search_term'].lower() in title.lower():
-                title = special_title['tidy_title']
-                break
-        
-        if title not in groups.keys():
-            groups[title] = { 'title': title, 'files': [], 'path': path, 'size': 0, 'episodes': set() }
-            
-        if episode_number:
-            groups[title]['episodes'].add(episode_number)
-            
-        file = { 'name': entry, 'path': path, 'size': os.stat(path).st_size // 1024**2 }
-        
-        groups[title]['files'].append(file)
-        groups[title]['size'] += file['size']
-        
-    for group in sorted(groups.values(), reverse = True, key = lambda x: x['size']):
-        text_tag = group['title']
-        episodes = list(group['episodes'])
-        if episodes:
-            ranges = []
-            index_a = 0
-            for index_b, (a, b) in enumerate(zip(episodes, episodes[1:] + [episodes[-1]])):
-                if b - a != 1:
-                    ranges.append((index_a, index_b))
-                    index_a = index_b + 1
-            text_tag += ' (%s)' % ', '.join('%d~%d' % (episodes[index_a], episodes[index_b]) if index_b - index_a else str(episodes[index_a]) for index_a, index_b in ranges)
-        
-        group_id = tree.insert('', END, text = text_tag, values = (len(group['files']), group['size']))
-        GROUP_TO_FILE_ID[group_id] = {}
-        
-        for file in group['files']:
-            file_id = tree.insert(group_id, END, text = file['name'], values = (1, file['size']))
-            GROUP_TO_FILE_ID[group_id][file_id] = file
-            FILE_TO_GROUP_ID[file_id] = group_id
-
-def on_tree_selection_update(event):
-    global SELECTED_GROUPS, SELECTED_FILES
-    SELECTED_GROUPS, SELECTED_FILES = [], []
-    for item_id in tree.selection():
-        if item_id in GROUP_TO_FILE_ID.keys():
-            SELECTED_GROUPS.append(item_id)
-        elif item_id in FILE_TO_GROUP_ID.keys():
-            SELECTED_FILES.append(item_id)
-    SELECTED_FILES = [sf for sf in SELECTED_FILES if FILE_TO_GROUP_ID[sf] not in SELECTED_GROUPS]
-    console_text.set('Selected %d groups and %d individual files.' % (len(SELECTED_GROUPS), len(SELECTED_FILES)))
-    
 tree.bind('<<TreeviewSelect>>', on_tree_selection_update)
+refresh_button.config(command = on_refresh_button_click)
+delete_button.config(command = on_delete_button_click)
 
 #
-refresh_button_click()
+on_refresh_button_click()
 root.mainloop()
